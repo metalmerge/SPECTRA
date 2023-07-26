073531228c46d40570dbcfb01998e83e67aac01c
Metalmerge <ermakovd06@gmail.com>
2023-07-26 02:19:51 -0400

    .3231

--- trainer.py
+++ trainer.py
@@ -5,6 +5,8 @@ from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 from PIL import Image
 import ssl
+import torch.optim.lr_scheduler as lr_scheduler
+
 ssl._create_default_https_context = ssl._create_default_https_context = ssl._create_unverified_context
 
 # Set the device (GPU or CPU)
@@ -13,8 +15,12 @@ print(device)
 
 # Define data transformations
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # Resize images to a fixed size
-    transforms.ToTensor(),         # Convert images to tensors
+    transforms.RandomResizedCrop(224),   # Randomly crop and resize images
+    transforms.RandomHorizontalFlip(),   # Randomly flip images horizontally
+    transforms.RandomVerticalFlip(),     # Randomly flip images vertically
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly adjust color
+    transforms.RandomRotation(10),       # Randomly rotate images by up to 10 degrees
+    transforms.ToTensor(),               # Convert images to tensors
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize image values
 ])
 
@@ -23,7 +29,7 @@ data_path = "/Users/dimaermakov/solar-Panel-Dataset"
 train_dataset = datasets.ImageFolder(data_path, transform=transform)
 
 # Create data loaders
-batch_size = 16
+batch_size = 16 #8 or 16 bigger better
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
 # Load the base model (ResNet50) with the pre-trained weights using the 'weights' parameter
@@ -37,12 +43,16 @@ model = base_model.to(device)
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-learning_rate = 0.001
+learning_rate = .0001 #0.001 or 0.0001 lower better
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+step_size = 5  # Reduce the learning rate every 5 epochs
+gamma = 0.1    # Reduce the learning rate by a factor of 0.1
+scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
 # Training loop
 false_positives = []
-num_epochs = 10   # Increase the number of epochs for better accuracy
+num_epochs = 20 # 10 or 20 bigger better
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
@@ -61,18 +71,24 @@ for epoch in range(num_epochs):
             predicted_classes = torch.argmax(outputs, dim=1)
             false_positive_mask = (predicted_classes != labels)
             false_positive_indices = (batch_idx * batch_size) + torch.nonzero(false_positive_mask).squeeze()
-            false_positives.extend(false_positive_indices.cpu().numpy().tolist())
             model.train()
 
-            # Print filenames of false positive images
-            if(num_epochs > 8):
-                for idx in false_positive_indices.cpu().numpy().tolist():
-                    print("False positive image filename:", train_dataset.imgs[idx][0])
+            # Check if false_positive_indices is not an integer
+            # if not isinstance(false_positive_indices, int):
+            #     false_positives.extend(false_positive_indices.cpu().numpy().tolist())
+            #     # Print filenames of false positive images
+            #     if epoch > 8:
+            #         if false_positive_indices.numel() > 1:
+            #             for idx in false_positive_indices.cpu().numpy().tolist():
+            #                 print("False positive image filename:", train_dataset.imgs[idx][0])
+            #         else:
+            #             print("False positive image filename:", train_dataset.imgs[false_positive_indices.item()][0])
 
         # Print progress every 5 batches
         if batch_idx % 5 == 4:
             print(f"Epoch {epoch + 1}/{num_epochs}, Batch {batch_idx + 1}/{len(train_loader)}, Loss: {running_loss / 5:.4f}")
             running_loss = 0.0
+    scheduler.step()
 
 # Save the trained model
 save_model = input("Do you want to save the trained model? (y/n): ").lower()
@@ -93,16 +109,16 @@ def predict_image(image_path):
         class_index = predicted_class.item()
         class_name = train_dataset.classes[class_index]
     return class_name
-def get_false_positive_images(dataset, false_positive_indices):
-    false_positive_images = [dataset[idx][0] for idx in false_positive_indices]
-    return false_positive_images
+# def get_false_positive_images(dataset, false_positive_indices):
+#     false_positive_images = [dataset[idx][0] for idx in false_positive_indices]
+#     return false_positive_images
 
 
 # Example usage
-false_positive_images = get_false_positive_images(train_dataset, false_positives)
+# false_positive_images = get_false_positive_images(train_dataset, false_positives)
 
 # Example usage
-print("Number of false positive images during training:", len(false_positive_images))
-image_path = "/Users/dimaermakov/Downloads/solar-Panel-Dataset/Clean/spotless-and-chemical-free-solar-panel-cleaning.jpg"
-predicted_class = predict_image(image_path)
-print("Predicted class:", predicted_class)
+# print("Number of false positive images during training:", len(false_positive_images))
+# image_path = "/Users/dimaermakov/Downloads/solar-Panel-Dataset/Clean/spotless-and-chemical-free-solar-panel-cleaning.jpg"
+# predicted_class = predict_image(image_path)
+# print("Predicted class:", predicted_class)
