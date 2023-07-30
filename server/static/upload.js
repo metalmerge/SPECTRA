9ec1a398597a057fe92c6f89d5654ab91d178dc2
Metalmerge <ermakovd06@gmail.com>
2023-07-30 11:07:45 -0400

    multiple files

--- server/static/upload.js
+++ server/static/upload.js
@@ -1,11 +1,19 @@
 let form = document.forms.namedItem("fileinfo");
-let lastImage;
+let lastImages = [];
 
 form.addEventListener('submit', function (ev) {
-  ev.preventDefault()
+  ev.preventDefault();
 
   document.getElementById("loader").style.visibility = "visible";
   let formdata = new FormData(form);
+
+  // Get all the uploaded files
+  const uploadedFiles = form.elements["file"].files;
+
+  // Append each file to the formdata separately
+  for (let i = 0; i < uploadedFiles.length; i++) {
+    formdata.append("file", uploadedFiles[i]);
+  }
 
   let requestOptions = {
     method: 'POST',
@@ -17,33 +25,51 @@ form.addEventListener('submit', function (ev) {
   fetch(`${loc.protocol}//${loc.hostname}:${loc.port}/predict`, requestOptions)
     .then(response => response.text().then((result => {
       console.log(result);
-      DisplayResult(result)
+      DisplayResult(result, uploadedFiles);
       document.getElementById("loader").style.visibility = "hidden";
     })));
 });
 
-//On Image Uploaded
+// On Images Uploaded
 form.addEventListener('change', function (ev) {
-  lastImage = URL.createObjectURL(ev.target.files[0]);
-  document.getElementById("uploadedImg").src = lastImage
-})
+  lastImages = [];
 
-function DisplayResult(result) {
+  // Get all the uploaded files
+  const uploadedFiles = form.elements["file"].files;
 
-  let resultDiv = document.getElementById("results")
+  for (let i = 0; i < uploadedFiles.length; i++) {
+    lastImages.push(URL.createObjectURL(uploadedFiles[i]));
+  }
 
-  let divContainer = document.createElement("div")
-  divContainer.className = "result"
+  // Display all the uploaded images
+  const imageContainer = document.getElementById("uploadedImages");
+  imageContainer.innerHTML = ""; // Clear previous images
 
-  let divImage = document.createElement("div")
-  divImage.className = "image"
-  divImage.style.backgroundImage = "url('" + lastImage + "')";
-  
-  let divText = document.createElement("div")
-  divText.appendChild(document.createTextNode(result))
-  divText.className = "text"
+  for (let i = 0; i < lastImages.length; i++) {
+    let imgElement = document.createElement("img");
+    imgElement.src = lastImages[i];
+    imgElement.className = "uploaded-img";
+    imageContainer.appendChild(imgElement);
+  }
+});
 
-  divContainer.appendChild(divImage)
-  divContainer.appendChild(divText)
-  resultDiv.prepend(divContainer)
+function DisplayResult(result, uploadedFiles) {
+  let resultDiv = document.getElementById("results");
+
+  for (let i = 0; i < uploadedFiles.length; i++) {
+    let divContainer = document.createElement("div");
+    divContainer.className = "result";
+
+    let divImage = document.createElement("div");
+    divImage.className = "image";
+    divImage.style.backgroundImage = "url('" + lastImages[i] + "')";
+
+    let divText = document.createElement("div");
+    divText.appendChild(document.createTextNode(result));
+    divText.className = "text";
+
+    divContainer.appendChild(divImage);
+    divContainer.appendChild(divText);
+    resultDiv.prepend(divContainer);
+  }
 }
