f15b46aad11afc4e95c519fbb7a57f110f640731
Metalmerge <ermakovd06@gmail.com>
2023-08-10 00:18:31 -0400

    format and todo

--- infrared_trainer.py
+++ infrared_trainer.py
@@ -2,22 +2,35 @@ from ultralytics import YOLO
 from PIL import Image
 import cv2
 import torch
+
 # Load a model
-model = YOLO('yolov8n.yaml')  # build a new model from YAML
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO("yolov8n.yaml")  # build a new model from YAML
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.yaml").load("yolov8n.pt")  # build from YAML and transfer weights
 # best_path = '/Users/dimaermakov/SPECTRA/runs/detect/train3/weights/best.pt'
 # model = YOLO(best_path)  # load a custom model
-#TODO:
-#figure out how to usee model onnx
+
+
+# TODO:
+# figure out imgsz
+#edit video
+#make this possible on front end
 
 # Train the model
-test = model.train(data='/Users/dimaermakov/Downloads/SolarPanelAI.v1i.yolov8/data.yaml', epochs=30, patience=10, imgsz=640, verbose=True, device='cpu', weight_decay=.0005)
+test = model.train(
+    data="/Users/dimaermakov/Downloads/SolarPanelAI.v1i.yolov8/data.yaml",
+    epochs=36,
+    patience=10,
+    imgsz=640,
+    verbose=True,
+    device="cpu",
+    weight_decay=0.0005,
+)
 
 
-# results = model(['/Users/dimaermakov/Downloads/SolarPanelAI.v1i.yolov8/test/images/82_jpeg.rf.777821a1868148596a8cafa2cc4d7193.jpg', '/Users/dimaermakov/Downloads/Solar Panel.v1-solardataset.yolov8/test/images/Thermal_Solar_508_jpg.rf.c982e0e42970e383ccd77c8d4fa36f86.jpg'])  # results list
 
 # Validate the model
+# results = model(['/Users/dimaermakov/Downloads/SolarPanelAI.v1i.yolov8/test/images/82_jpeg.rf.777821a1868148596a8cafa2cc4d7193.jpg', '/Users/dimaermakov/Downloads/Solar Panel.v1-solardataset.yolov8/test/images/Thermal_Solar_508_jpg.rf.c982e0e42970e383ccd77c8d4fa36f86.jpg'])  # results list
 # metrics = model.val()  # no arguments needed, dataset and settings remembered
 # metrics.box.map    # map50-95
 # metrics.box.map50  # map50
@@ -30,11 +43,11 @@ test = model.train(data='/Users/dimaermakov/Downloads/SolarPanelAI.v1i.yolov8/da
 #     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
 #     im.show()  # show image
 #     im.save('results.jpg')  # save image
-    # print(r.boxes,r.boxes.xyxy, r.probs)
+# print(r.boxes,r.boxes.xyxy, r.probs)
 
-video_path = '/Users/dimaermakov/Downloads/videoplayback (1).webm'
 
 # Run inference on the source
+# video_path = "/Users/dimaermakov/Downloads/videoplayback (1).webm"
 # cap = cv2.VideoCapture(video_path)
 
 # Loop through the video frames
@@ -63,6 +76,8 @@ video_path = '/Users/dimaermakov/Downloads/videoplayback (1).webm'
 # cap.release()
 # cv2.destroyAllWindows()
 
-model.export(format='onnx')
-torch.save(model, "infrared_model.pth")
-# torch.save(model, "savetesting2.pt")
+
+
+
+model.export(format="onnx")
+# torch.save(model, "infrared_model.pth")
