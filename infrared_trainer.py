d6fa544260547f6afd60d7ed0b04c892494d5ed7
Metalmerge <ermakovd06@gmail.com>
2023-08-16 03:03:50 -0400

    image folder is now taken

--- infrared_trainer.py
+++ infrared_trainer.py
@@ -21,12 +21,18 @@ def train_model(epoch_num):
     return model
 
 
-def validate_and_visualize(model, image_array):
-    results = model(image_array)  # Run validation
-    for r in results:
-        im_array = r.plot()  # Plot predictions
-        im = Image.fromarray(im_array[..., ::-1])  # Convert to RGB PIL image
-        im.show()  # Show image
+def validate_and_visualize(model, image_folder):
+    image_files = os.listdir(image_folder)
+    
+    for image_file in image_files:
+        image_path = os.path.join(image_folder, image_file)
+        image = Image.open(image_path)
+        results = model(image)  # Run validation
+        
+        for r in results:
+            im_array = r.plot()  # Plot predictions
+            im = Image.fromarray(im_array[..., ::-1])  # Convert to RGB PIL image
+            im.show()  # Show image
 
 
 def infer_and_save_video(model, video_path, output_path):
@@ -72,18 +78,12 @@ def main():
     elif train == 0:
         model = YOLO(best_path)  # Load custom model
 
-        image_array = [
-            "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images/468_jpeg.rf.7907654da74862b08c65c83f2f58e689.jpg",
-            "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images/50_jpeg.rf.a9237d9b3e87abdb25df228965a27c21.jpg",
-            "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images/56_jpeg.rf.78d9bdd3d8c01bf317b29cea45e36812.jpg",
-            "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images/82_jpeg.rf.777821a1868148596a8cafa2cc4d7193.jpg",
-            "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images/96_jpeg.rf.c2a6093ac882077421dc534bca4330c6.jpg",
-        ]
-        validate_and_visualize(model, image_array)
+        image_folder = "/Users/dimaermakov/Downloads/SolarPanelAI.493.yolov8/test/images"
+        validate_and_visualize(model, image_folder)
 
         video_path = "/Users/dimaermakov/Downloads/Thermography Solar Panel Video.mp4"
 
-        infer_and_save_video(model, video_path, "/Users/dimaermakov/Downloads/output493.mp4")
+        # infer_and_save_video(model, video_path, "("/Users/dimaermakov/Downloads/output493.mp4")
 
 if __name__ == "__main__":
     main()
